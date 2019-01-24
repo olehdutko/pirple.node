30793d514d2b35e8619fc81b06d25ab9497c02ee
Олег Дутко <odutko@MacBook-Pro-2.local>
2019-01-24 16:09:23 +0200

    Homework Assignment #1

---
+++ Homework Assignment_1/index.js
@@ -0,0 +1,88 @@
+/*
+ * Homework Assignment # 1
+ *
+ */
+
+// Dependencies
+var http = require('http');
+var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+
+ // Configure the server to respond to all requests with a string
+var server = http.createServer(function(req,res){
+
+  // Parse the url
+  var parsedUrl = url.parse(req.url, true);
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  var queryStringObject = parsedUrl.query;
+  var method = req.method.toLowerCase();
+  var headers = req.headers;
+
+  // Get the payload,if any
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
+  req.on('data', function(data) {
+      buffer += decoder.write(data);
+  });
+
+  req.on('end', function() {
+      buffer += decoder.end();
+
+      // Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
+      var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+      // Construct the data object to send to the handler
+      var data = {
+        'trimmedPath' : trimmedPath,
+        'queryStringObject' : queryStringObject,
+        'method' : method,
+        'headers' : headers,
+        'payload' : buffer
+      };
+
+      // Route the request to the handler specified in the router
+      chosenHandler(data,function(statusCode,payload){
+
+        // Use the status code returned from the handler, or set the default status code to 200
+        statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+        // Use the payload returned from the handler, or set the default payload to an empty object
+        payload = typeof(payload) == 'object'? payload : {};
+
+        // Convert the payload to a string
+        var payloadString = JSON.stringify(payload);
+
+        // Return the response
+        res.setHeader('Content-Type', 'application/json');
+        res.writeHead(statusCode);
+        res.end(payloadString);
+        // console.log("Returning this response: ",statusCode,payloadString);
+
+      });
+
+  });
+});
+
+// Start the server
+server.listen(4000,function(){
+  console.log('Server started....');
+});
+
+// Define all the handlers
+var handlers = {};
+
+handlers.hallo = function(data,callback){
+    // callback(200, {'name':'This is \"Hallo handler\". Google URL is here -  http://google.com/'});
+    data.name = 'This is \"Hallo handler\". Google URL is here -  http://google.com/';
+    callback(200, data);
+};
+
+// Not found handler
+handlers.notFound = function(data,callback){
+  callback(404);
+};
+
+var router = {
+  'hallo' : handlers.hallo
+};
